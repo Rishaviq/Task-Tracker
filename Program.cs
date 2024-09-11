0b533e56bfc61c89208049507a0e02b3b339aef2
Rishaviq <juliqnbg@gmail.com>
2024-09-11 10:25:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks abc= new Tasks("aaa");
+           
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Tracker
+{
+    internal class Tasks
+    {
+
+        private string _name { get; set; }
+        private string _description { get; set; }
+        
+        private string progress;
+
+       public Tasks(string name, string description)
+        {
+            _description = description;
+            _name = name;
+            progress = "no progress";
+        }
+        public Tasks(string name)
+        {
+            _description ="";
+            _name = name;
+            progress = "no progress";
+        }
+    }
+}
